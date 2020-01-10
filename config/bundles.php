e63152650aa3f28bdac5794736bdeff29a281703
Alexg78bis <alexg78bis@gmail.com>
2020-01-10 23:21:49 +0100

    - Fixture installation - Faker installation - User fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private const PASSWORD = 'password';
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $userPasswordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    /**
+     * Fixture function
+     * @param ObjectManager $manager
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public
+    function load(ObjectManager $manager)
+    {
+        $this->generateAdmin($manager);
+        $this->generateUsers($manager);
+
+        $manager->flush();
+    }
+
+    private function generateAdmin(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        $admin = new User();
+        $admin->setEmail('admin@mail.com');
+        $admin->setBirthdate($faker->dateTime("2001-01-01"));
+        $admin->setFirstname($faker->firstName);
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->userPasswordEncoder->encodePassword($admin, self::PASSWORD));
+
+        $manager->persist($admin);
+    }
+
+
+    private function generateUsers(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail($faker->safeEmail);
+            $user->setBirthdate($faker->dateTime("now"));
+            $user->setFirstname($faker->firstName);
+            $user->setPassword($this->userPasswordEncoder->encodePassword($user, self::PASSWORD));
+
+            $manager->persist($user);
+        }
+    }
+
+
+}
